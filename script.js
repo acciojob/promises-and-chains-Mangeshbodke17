a4f98a7e0da93e2278ffe51568ee83a4e29c9d48
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-27 12:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+
+
+
+let age=document.querySelector("#age");
+
+let name=document.querySelector("#name");
+
+let btn=document.querySelector("button");
+
+
+btn.addEventListener("click",(e)=>{
+
+    e.preventDefault();
+
+    
+    if((age.value=="") || (name.value=="")){
+        alert("Please enter valid details.")
+    }
+
+else if(age.value>18){
+
+
+ 
+   setTimeout(() => {
+    alert("Welcome, . You can vote.");
+}, 4000);
+
+    }
+
+    else{
+        setTimeout(() => {
+    alert("Oh sorry . You aren't old enough.");
+}, 4000);
+
+    }
+
+
+})
+
